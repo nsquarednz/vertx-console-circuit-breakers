15817a268a4a6dddf6a0af092bcf30602d6004c1
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-02 20:12:36 -0600

    Address endpoint

--- src/main/java/in/yunyul/vertx/console/circuitbreakers/CircuitBreakersHandler.java
+++ src/main/java/in/yunyul/vertx/console/circuitbreakers/CircuitBreakersHandler.java
@@ -1,12 +1,15 @@
 package in.yunyul.vertx.console.circuitbreakers;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
 public class CircuitBreakersHandler {
+    private static final String JSON_CONTENT_TYPE = "application/json";
+
     public CircuitBreakersHandler(Vertx vertx, Router router, String basePath, String circuitBreakerAddress) {
         // Set up streaming
         SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
@@ -17,5 +20,8 @@ public class CircuitBreakersHandler {
                 .addOutboundPermitted(breakerPermitted);
         sockJSHandler.bridge(options);
         router.route(basePath + "/circuitbreakerproxy/*").handler(sockJSHandler);
+        router.route(basePath + "/circuitbreakers/address").produces(JSON_CONTENT_TYPE).handler(ctx ->
+                ctx.response().putHeader("content-type", JSON_CONTENT_TYPE)
+                        .end(new JsonObject().put("address", circuitBreakerAddress).toString()));
     }
 }
