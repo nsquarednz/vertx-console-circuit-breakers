593bab4bd6ce823f3db31addc222b0de10743db2
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-03 09:14:58 -0600

    Index breakers by name

--- frontend/src/CircuitBreakers.vue
+++ frontend/src/CircuitBreakers.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div v-for="msg in msgs" :key="msg">{{ msg }}</div>
+        <div v-for="(breaker, name) in breakers" :key="name">{{ breaker }}</div>
     </div>
 </template>
 
@@ -10,18 +10,18 @@ import EventBus from 'vertx3-eventbus-client';
 export default {
     data() {
         return {
-            msgs: []
+            breakers: {}
         }
     },
     beforeMount() {
-
         this.$http.get(window.location.pathname + '/circuitbreakers/address')
             .then(response => response.json())
             .then(response => {
                 this.eb = new EventBus(window.location.pathname + '/circuitbreakerproxy');
                 this.eb.onopen = () => {
                     this.eb.registerHandler(response.address, (e, m) => {
-                        this.msgs.push(m.body);
+                        const breaker = m.body;
+                        this.$set(this.breakers, breaker.name, breaker);
                     });
                 };
             });
