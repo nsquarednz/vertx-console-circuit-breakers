2d01289dee55e614eee0096dceef55b55f876714
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-03 09:26:42 -0600

    Faster initialization with promises

--- frontend/src/CircuitBreakers.vue
+++ frontend/src/CircuitBreakers.vue
@@ -8,23 +8,29 @@
 import EventBus from 'vertx3-eventbus-client';
 
 export default {
+    name: 'Circuit Breakers',
     data() {
         return {
             breakers: {}
         }
     },
     beforeMount() {
-        this.$http.get(window.location.pathname + '/circuitbreakers/address')
-            .then(response => response.json())
-            .then(response => {
-                this.eb = new EventBus(window.location.pathname + '/circuitbreakerproxy');
-                this.eb.onopen = () => {
-                    this.eb.registerHandler(response.address, (e, m) => {
-                        const breaker = m.body;
-                        this.$set(this.breakers, breaker.name, breaker);
-                    });
-                };
+        const getAddress = this.$http.get(window.location.pathname + '/circuitbreakers/address')
+            .then(response => response.json());
+        const openEventBus = new Promise((resolve, reject) => {
+            this.eb = new EventBus(window.location.pathname + '/circuitbreakerproxy');
+            this.eb.onopen = () => resolve(true);
+            this.eb.onerror = err => reject(err);
+        });
+        Promise.all([getAddress, openEventBus]).then(values => {
+            this.eb.registerHandler(values[0].address, (e, m) => {
+                const breaker = m.body;
+                this.$set(this.breakers, breaker.name, breaker);
             });
+        });
+    },
+    beforeDestroy() {
+        this.eb.close();
     }
 }
 </script>
