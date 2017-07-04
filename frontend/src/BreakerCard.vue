08af1a89ce3cefdbac9529dc0f6d2684a3112251
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-04 16:48:43 -0600

    reeeeeeeeeee

--- frontend/src/BreakerCard.vue
+++ frontend/src/BreakerCard.vue
@@ -2,8 +2,8 @@
     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
         <div class="card-pf card-pf-view card-pf-view-select card-pf-view-single-select breaker-card">
             <div class="card-pf-body">
-                <h2 class="card-pf-title">{{ breaker.name }}</h2>
-                {{ breaker.state }}
+                <h2 class="card-pf-title breaker-name">{{ breaker.name }}</h2>
+                <div class="breaker-state">{{ breaker.state.replace('_', ' ') }}</div>
             </div>
         </div>
     </div>
@@ -21,4 +21,19 @@ export default {
 .breaker-card .card-pf-body {
     height: 150px;
 }
+
+.breaker-name {
+    font-size: 16px;
+    margin-top: 0;
+}
+
+.breaker-state {
+    float: right;
+    font-weight: bold;
+}
+
+.breaker-name, .breaker-state {
+    display: inline-block;
+}
+
 </style>
