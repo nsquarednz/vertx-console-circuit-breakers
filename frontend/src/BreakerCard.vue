045c71fb5ecb9900099ef1735fe6382c426cadcb
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-04 21:46:26 -0600

    Hmm

--- frontend/src/BreakerCard.vue
+++ frontend/src/BreakerCard.vue
@@ -4,8 +4,10 @@
             <div class="card-pf-body">
                 <h2 class="card-pf-title breaker-name">{{ breaker.name }}</h2>
                 <div class="breaker-state">{{ breaker.state.replace('_', ' ') }}</div>
-                <div class="rate-chart">
-                    <pf-sparkline :tooltipContents="tooltipContents" :maxDisplayed="20" :data="operationRate"></pf-sparkline>
+                <div class="breaker-info">
+                    <div class="rate-chart">
+                        <pf-sparkline :tooltipContents="tooltipContents" :maxDisplayed="20" :data="operationRate"></pf-sparkline>
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,7 +22,7 @@ export default {
         breaker: Object
     },
     created() {
-        this.tooltipContents = { contents: d => '<span class="c3-tooltip-sparkline">' + abbreviate(d[0].value, 1) + ' Ops/sec</span>'}
+        this.tooltipContents = { contents: d => '<span class="c3-tooltip-sparkline">' + abbreviate(d[0].value, 1) + ' Ops/sec</span>' }
     },
     computed: {
         operationRate() {
@@ -31,18 +33,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$card-margin: 10px;
+$card-height: 150px;
+
+.breaker-card {
+    padding: 0;
+}
+
 .breaker-card .card-pf-body {
-    height: 150px;
+    height: $card-height;
+    padding: 0;
+    margin-top: $card-margin;
 }
 
 .breaker-name {
     font-size: 16px;
     margin-top: 0;
+    margin-left: $card-margin;
 }
 
 .breaker-state {
     float: right;
     font-weight: bold;
+    margin-right: $card-margin;
 }
 
 .breaker-name,
@@ -50,9 +63,19 @@ export default {
     display: inline-block;
 }
 
+.breaker-info {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    height: $card-height - $card-margin;
+    padding-bottom: 6px;
+}
+
 .rate-chart {
-    position: absolute;
-    bottom: 0;
-    left: 0;
+    margin-top: auto;
 }
 </style>
